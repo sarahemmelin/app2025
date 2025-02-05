393894d17890665202c5d9d5a84169cc304459e2
sarahemmelin <sarahemmelin@gmail.com>
2025-02-05 17:19:03 +0100

    Gjennomgår forelesning på nytt og programmerer sammen med Christian sin recording

--- server.mjs
+++ server.mjs
@@ -4,17 +4,23 @@ import express from 'express';
 import path from 'path';
 import deckRoutes from './routes/deckRoutes.mjs';
 import poetryRoutes from './routes/poetryRoutes.mjs';
+import log from './modules/log.mjs';
+
 
 const server = express();
 const port = process.env.PORT || 3000;
 
+
 server.set('port', port);
 server.use(express.static('public/DeckOfCards'));
 server.use(express.json());
+server.use(log);
 
-server.get("/", (req, res) => {
+function serveDeckPage(req, res) {
     res.sendFile(path.resolve('public/DeckOfCards/index.html'));
-});
+}
+
+server.get("/", serveDeckPage);
 
 server.use('/temp', deckRoutes);
 server.use('/tmp', poetryRoutes);
