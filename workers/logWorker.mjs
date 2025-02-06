affa3c06628b59e1228b7bd9ca8f8effd3fd1f18
sarahemmelin <sarahemmelin@gmail.com>
2025-02-06 21:36:12 +0100

    1:29:08 Har lagt til at ansi-koder ikke vises i de lagrede filene, men fortsatt vises i konsoll-loggen.

--- workers/logWorker.mjs
+++ workers/logWorker.mjs
@@ -1,11 +1,16 @@
 import { parentPort } from "node:worker_threads";
 import fs from "node:fs/promises";
 
+const removeAnsiCodes = (text) => text.replace(/\x1B\[[0-9;]*m/g, "");
+
 parentPort.on('message', async (logText) => {
 
-    const { logStatement } = logText;
-    
+    let logStatement = logText.logStatement;
     console.log("[Worker]", logStatement);
+
+    if (typeof logStatement === "string") {
+        logStatement = removeAnsiCodes(logStatement);
+    }
     
     try {
         await fs.appendFile("./logs/log.csv", logStatement + "\n");
