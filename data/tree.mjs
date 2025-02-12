78208941496b95bb7cbd8906e3a1a3e8b2528c32
sarahemmelin <sarahemmelin@gmail.com>
2025-02-12 12:08:28 +0100

    tree, init, treeAPI og eksempel (dummy) på et prosjekttre er opprettet for å klargjøre for ny oppgave i App2.

--- data/tree.mjs
+++ data/tree.mjs
@@ -8,5 +8,16 @@ const Node = function(data,...connections) { //...rest operator: uendelig mengde
     return {data,connections:[...connections]};  //...spread operator: Samler sammen resten av argumentene i en array.
 }
 
+
+
+export function saveTree(tree) {
+    return JSON.stringify(tree, null, 3);
+}
+
+export function inflateTree(data){
+    return JSON.parse(data);
+}
+
+
 export {Tree, Node};
 
--- routes/treeAPI.mjs
+++ routes/treeAPI.mjs
@@ -1,7 +1,17 @@
 import express from 'express';
+import { Tree, Node } from '../data/tree.mjs';
 
 const treeRouter = express.Router();
 treeRouter.use(express.json());
 
+const tree = Tree(Node(""));
+
+treeRouter.use(express.json());
+
+treeRouter.get("/", (req, res, next) => {
+
+    res.json(tree);
+
+});
 
 export { treeRouter };
