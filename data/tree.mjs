83cff80a0b140fcd86facbd9bb9bd2d84cb041d8
sarahemmelin <sarahemmelin@gmail.com>
2025-02-21 22:23:17 +0100

    Fjernet kommentarer fra forelesning

--- data/tree.mjs
+++ data/tree.mjs
@@ -1,11 +1,11 @@
-const Tree = function (root) { //En stor bokstav: Forteller at det er en type.
+const Tree = function (root) { 
 
     return {root};
 
 }
 
-const Node = function(data,...connections) { //...rest operator: uendelig mengder av argumenter: Den tar alt som kommer inn og legger det i en array for deg.
-    return {data,connections:[...connections]};  //...spread operator: Samler sammen resten av argumentene i en array.
+const Node = function(data,...connections) { 
+    return {data,connections:[...connections]};
 }
 
 
