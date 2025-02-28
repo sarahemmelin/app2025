241a8068b1eeeb97cb38991edb9a7d823d0c2441
sarahemmelin <sarahemmelin@gmail.com>
2025-02-28 21:45:14 +0100

    Har fjernet kode som ikke brukes (ryddet).

--- server.mjs
+++ server.mjs
@@ -5,17 +5,11 @@ dotenv.config();
 import express from 'express';
 // import path from 'path';
 import shopAPI from './routes/shopAPI.mjs';
-// import deckRoutes from './routes/deckRoutes.mjs';
-// import poetryRoutes from './routes/poetryRoutes.mjs';
 // import log from './modules/log.mjs';
 // import { LOGG_LEVELS, eventLogger } from './modules/log.mjs';
 import { vanguard } from './modules/vanguard.mjs';
-// import { startBossFight } from './simulatorbots/bossFight.mjs';
 // import { updateSession } from './modules/session.mjs';
 // import { startSession } from './modules/session.mjs';
-// import { treeRouter } from './routes/treeAPI.mjs';
-// import { questLogRouter } from './routes/questLogAPI.mjs';  
-// import { userRouter } from './routes/userAPI.mjs';
 
 const server = express();
 server.use(express.json());
@@ -38,13 +32,12 @@ server.use((req, res, next) => {
 
 server.use("/shop/", shopAPI);
 server.get("/", (req, res) => {
-    res.send("Gå til /shop for å hente produkter!");
+    res.sendFile("index.html", { root: "public" });
 });
 
 // server.use(updateSession);
 // server.use(startSession);
 // server.use(logger);
-// server.get("/", serveDeckPage); 
 // server.use(express.static('public/DeckOfCards'));
 // server.use("/tree/", treeRouter);
 
@@ -54,27 +47,9 @@ server.get("/", (req, res) => {
 // server.use('/', deckRoutes);
 // server.use('/tmp', poetryRoutes);
 
-// {
-//     "navn": "Winsor & Newton Gouache",
-//     "kategori": ["Maling", "Gouache"],
-//     "pris": 129.90,
-//     "lager": 20,
-//     "farge": "Blå",
-//     "pigment": "PB15",
-//     "sku": "WN-GOUACHE-BLUE"
-// }
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
-    // console.log("VANGUARD IS READY TO DEFEND!");
-    // startBossFight(); Denne fungerer ikke atm. 
 });
-
-
-
-// function serveDeckPage(req, res) {
-//     eventLogger("Serving deck page");
-//     res.sendFile(path.resolve('public/DeckOfCards/index.html'));
-// }
 
 export { server };  
