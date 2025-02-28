bb7510c58b803d4c54ad77dfcf33033cff6ee972
sarahemmelin <sarahemmelin@gmail.com>
2025-02-28 21:48:42 +0100

    Har lagt til mapper og mappestruktur i klient

---
+++ public/js/app.mjs
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("/serviceWorker.js")
+            .then(() => console.log("Service Worker registrert!"))
+            .catch(error => console.error("Service Worker-feil:", error));
+    }
+});
+
