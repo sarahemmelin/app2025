007c69ddf94eb0a4ea8b0fe1eeb929b3a6fd81c0
sarahemmelin <sarahemmelin@gmail.com>
2025-02-28 21:52:20 +0100

    Fjernet event - listener.

--- public/js/app.mjs
+++ public/js/app.mjs
@@ -1,9 +1,5 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("/serviceWorker.js")
-            .then(() => console.log("Service Worker registrert!"))
-            .catch(error => console.error("Service Worker-feil:", error));
-    }
-});
-
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/serviceWorker.js")
+        .then(() => console.log("✅ Service Worker registrert!"))
+        .catch(error => console.error("❌ Service Worker-feil:", error));
+}
