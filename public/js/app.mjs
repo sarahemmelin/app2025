808c407864678adad9c36c9fc4fd0cea9025c813
sarahemmelin <sarahemmelin@gmail.com>
2025-03-01 22:45:54 +0100

    Lagt til inititateApp() - funksjon som henter produkter.

--- public/js/app.mjs
+++ public/js/app.mjs
@@ -1,5 +1,16 @@
+import { fetchProducts } from "./api.mjs";
+
+
+async function initiateApp() {
+    const products = await fetchProducts();
+    console.log("Produkter:", products);
+}
+
+
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/serviceWorker.js")
         .then(() => console.log("Service Worker registrert!"))
         .catch(error => console.error("Service Worker-feil:", error));
 }
+
+initiateApp();
