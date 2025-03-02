487aa33b938cbce98186b32c71508765bb069ba3
sarahemmelin <sarahemmelin@gmail.com>
2025-03-02 14:24:54 +0100

    Henter produkter fra server

--- public/js/app.mjs
+++ public/js/app.mjs
@@ -6,6 +6,26 @@ async function initiateApp() {
     registerServiceWorker();
     const products = await fetchProducts();
     console.log("Produkter:", products);
-}
-
-initiateApp();
+    
+    const productsContainer = document.getElementById("products");
+    products.forEach(product => {
+      const productCard = document.createElement("product-card");
+      
+      productCard.setAttribute("navn", product.navn);
+      productCard.setAttribute("sku", product.sku);
+      productCard.setAttribute("lager", product.lager);
+      productCard.setAttribute("pris", product.pris);
+      productCard.setAttribute("beskrivelse", product.beskrivelse);
+      
+      if (product.farge) {
+        productCard.setAttribute("farge", product.farge);
+      }
+      if (product.pigmenter) {
+        productCard.setAttribute("pigmenter", Array.isArray(product.pigmenter) ? product.pigmenter.join(", ") : product.pigmenter);
+      }
+      
+      productsContainer.appendChild(productCard);
+    });
+  }
+  
+  initiateApp();
