b3f14e26dccd173d30ea6f0581fe3129278444cc
sarahemmelin <sarahemmelin@gmail.com>
2025-03-02 20:49:21 +0100

    Fjernet ikon fra konsollen.

--- workers/scribe.mjs
+++ workers/scribe.mjs
@@ -6,7 +6,7 @@ const removeAnsiCodes = (text) => text.replace(/\x1B\[[0-9;]*m/g, "");
 parentPort.on("message", async (logText) => {
     
     let logStatement = removeAnsiCodes(logText.logStatement);
-    console.log("📜 [Scribe Log]:", logStatement);
+    console.log("[Scribe Log]:", logStatement);
 
     try {
         await fs.appendFile("./logs/security.csv", logStatement + "\n");
