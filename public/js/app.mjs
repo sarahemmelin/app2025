6c2c6e89ae2c014176d20e12cb7a090d64294a0c
sarahemmelin <sarahemmelin@gmail.com>
2025-03-02 20:47:45 +0100

    Har flyttet "controllers" ut i egen fil --> productController.mjs

--- public/js/app.mjs
+++ public/js/app.mjs
@@ -1,38 +1,11 @@
-import { fetchProducts } from "./api.mjs";
 import { registerServiceWorker } from "./registerSW.mjs";
+import { initProductView } from "./productController.mjs";
 import "/components/product-card.mjs";
+import "/components/add-product-form.mjs";
 
 async function initiateApp() {
     registerServiceWorker();
-    const products = await fetchProducts();
+    await initProductView();
+}
 
-    const productsContainer = document.getElementById("products");
-
-    const filteredProducts = products.filter(product => product.id !== "0");
-    filteredProducts.forEach(product => {
-      const productCard = document.createElement("product-card");
-
-      if (!product.navn) {
-        console.warn ("produktet mangler navn, setter til 'Ukjent produkt'");
-        product.navn = "Ukjent produkt";
-      }
-      
-      productCard.setAttribute("id", product.id);
-      productCard.setAttribute("navn", product.navn);
-      productCard.setAttribute("sku", product.sku || "Ukjent SKU");
-      productCard.setAttribute("lager", product.lager || "0");
-      productCard.setAttribute("pris", product.pris || "0");
-      productCard.setAttribute("beskrivelse", product.beskrivelse || "Ingen beskrivelse");
-      
-      if (product.farge) {
-        productCard.setAttribute("farge", product.farge);
-      }
-      if (product.pigmenter) {
-        productCard.setAttribute("pigmenter", Array.isArray(product.pigmenter) ? product.pigmenter.join(", ") : product.pigmenter);
-      }
-      
-      productsContainer.appendChild(productCard);
-    });
-  }
-  
-  initiateApp();
+initiateApp();
