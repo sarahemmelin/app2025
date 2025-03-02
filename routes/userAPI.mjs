c65b89dd70514903b70607551d4d397df79f406b
sarahemmelin <sarahemmelin@gmail.com>
2025-03-02 20:48:53 +0100

    Har slettet filer som ikke er i bruk.

--- routes/userAPI.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const userRouter = express.Router();
-userRouter.use(express.json());
-
-export { userRouter };
-
-
-//kan lage egne snippets som man kan injeksere i dokumenter (eks. i html-filer) for å få enkel tilgang til kode.
