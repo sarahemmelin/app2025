2221fab73c349274dcc445a92958a096da51f4b5
sarahemmelin <sarahemmelin@gmail.com>
2025-03-04 22:42:53 +0100

    Ryddet kode

--- modules/auth.mjs
+++ modules/auth.mjs
@@ -3,7 +3,7 @@ const activeTokens = new Set();
 export function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     if (!authHeader) {
-        return res.status(401).json({ message: "Manglende autorisasjon." });
+        return res.status(403).json({ message: "Manglende autorisasjon." });
     }
 
     const token = authHeader.split(" ")[1];
@@ -11,9 +11,9 @@ export function authenticateToken(req, res, next) {
     if (!activeTokens.has(token)) {
         return res.status(403).json({ message: "Ugyldig eller utløpt token." });
     }
-
     next();
 }
+
 
 export function storeToken(token) {
     activeTokens.add(token);
