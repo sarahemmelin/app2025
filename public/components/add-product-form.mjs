ae9448073b1cf8cc6e3a177cacb46f9fefed299b
sarahemmelin <sarahemmelin@gmail.com>
2025-03-04 22:44:23 +0100

    Ryddet kode

--- public/components/add-product-form.mjs
+++ public/components/add-product-form.mjs
@@ -9,7 +9,7 @@ class AddProductForm extends HTMLElement {
       try {
         const response = await fetch('/templates/addProductForm.html');
         if (!response.ok) {
-          throw new Error(`Feil ved lasting av form-template: ${response.status}`);
+          throw new Error(`[ERROR] Feil ved lasting av form-template: ${response.status}`);
         }
         const text = await response.text();
         const wrapper = document.createElement('div');
@@ -20,25 +20,26 @@ class AddProductForm extends HTMLElement {
           this.shadowRoot.appendChild(clone);
           this.addListeners();
         } else {
-          console.error('Fant ikke <template> elementet i addProductForm.html');
+          console.error('[ERROR] Fant ikke <template> elementet i addProductForm.html');
         }
       } catch (error) {
-        console.error('Feil ved lasting av addProductForm template:', error);
+        console.error('[ERROR] Feil ved lasting av addProductForm template:', error);
       }
     }
   
     addListeners() {
       const form = this.shadowRoot.getElementById('addProductForm');
       if (form) {
+
         form.addEventListener('submit', async (e) => {
           e.preventDefault();
+
           const formData = new FormData(form);
           const newProduct = Object.fromEntries(formData.entries());
-          console.log('Nytt produkt:', newProduct);
-
           const token = localStorage.getItem('authToken');
+
           if (!token) {
-            console.error('Ingen token funnet! Brukeren er ikke logget inn.');
+            console.error('[ERROR] Ingen token funnet! Brukeren er ikke logget inn.');
             return;
           }
   
@@ -52,12 +53,11 @@ class AddProductForm extends HTMLElement {
               body: JSON.stringify(newProduct)
             });
             if (!response.ok) {
-              throw new Error(`Feil ved oppretting av produkt: ${response.status}`);
+              throw new Error(`[ERROR] Feil ved oppretting av produkt: ${response.status}`);
             }
             const result = await response.json();
-            console.log('Produkt lagt til:', result);
           } catch (error) {
-            console.error('Feil ved oppretting av produkt:', error);
+            console.error('[ERROR] Feil ved oppretting av produkt:', error);
           }
         });
       }
