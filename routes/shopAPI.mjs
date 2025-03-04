22351d092708d2a0b1d206741e84d786b50d0377
sarahemmelin <sarahemmelin@gmail.com>
2025-03-04 10:31:16 +0100

    Dekomponert og ryddet opp

--- routes/shopAPI.mjs
+++ routes/shopAPI.mjs
@@ -1,28 +1,14 @@
 import express from "express";
-import { authenticateAPIKey } from "../modules/auth.mjs";
+import { authenticateToken } from "../modules/auth.mjs";
 import { getAllProducts, getProduct, createProduct, deleteProduct } from "../controllers/shopController.mjs";
-import { vanguard } from "../modules/vanguard.mjs"; 
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
 router.get("/products", getAllProducts);
-router.get("/product-metadata", (req, res) => {
-  const metadata = {
-    produktStruktur: {
-      generell: ["id", "sku", "navn", "kategori", "pris", "lager", "beskrivelse"],
-      maling: ["farge", "pigmenter"]
-      // Tanke: Bruke kategori som custom - egenskaper til spesifikke produktgrupper. Eksempel: papir: ["papirtype", "gramvekt"]
-    }
-  };
-  res.json(metadata);
-});
 router.get("/:id", getProduct);
 
-
-router.post("/", (req, res, next) => {
-    if (vanguard.skills[0].use(req, res)) next();
-  }, authenticateAPIKey, createProduct);
-router.delete("/:id", authenticateAPIKey, deleteProduct);
+router.post("/", authenticateToken, createProduct);
+router.delete("/:id", authenticateToken, deleteProduct);
 
 export default router;
