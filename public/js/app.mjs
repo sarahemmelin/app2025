293dc8e4d54acc7f4d5d02327e6f5810eb3d5251
sarahemmelin <sarahemmelin@gmail.com>
2025-03-07 11:52:48 +0100

    Omstrukturerer for bedre oversikt.

--- public/js/app.mjs
+++ public/js/app.mjs
@@ -1,14 +1,14 @@
 import { registerServiceWorker } from "./registerSW.mjs";
-import { initProductView } from "./productController.mjs";
-import { initLoginView } from "./loginController.mjs";
+import { handleRouting } from "./router/router.mjs";
+import { checkLoginStatus } from "./controllers/loginController.mjs";
 import "/components/product-card.mjs";
 import "/components/add-product-form.mjs";
 import "/components/login-component.mjs";
 
 async function initiateApp() {
     registerServiceWorker();
-    await initProductView();
-    initLoginView();
+    checkLoginStatus();
+    handleRouting();
 }
 
 initiateApp();
