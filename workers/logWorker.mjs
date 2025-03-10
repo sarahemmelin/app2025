3d727da2b515389b38e30d47f2acec9bce41b316
sarahemmelin <sarahemmelin@gmail.com>
2025-03-10 15:26:00 +0100

    Fikset en konstant, derp

--- workers/logWorker.mjs
+++ workers/logWorker.mjs
@@ -3,7 +3,7 @@ import fs from "node:fs/promises";
 import path from "node:path";
 
 
-CONST_LOG_DIR = "./logs";
+const LOG_DIR = "./logs";
 const LOG_FILE = path.join(LOG_DIR, "log.csv");
 const removeAnsiCodes = (text) => text.replace(/\x1B\[[0-9;]*m/g, "");
 
