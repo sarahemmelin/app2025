887701049ea788902e74192c367ca1b0cc3ba175
sarahemmelin <sarahemmelin@gmail.com>
2025-03-11 16:03:26 +0100

    Ryddet i serverfilen, har lagt til DEBUG_MODE

--- server.mjs
+++ server.mjs
@@ -5,30 +5,33 @@ import authAPI from './routes/authAPI.mjs';
 import { vanguard } from './modules/vanguard.mjs';
 import path from 'path';
 import log, { eventLogger, LOGG_LEVELS } from './modules/log.mjs';
+import { DEBUG_MODE } from './config/debug.mjs';
+
+if (DEBUG_MODE) console.log("DEBUG NODE_ENV:", process.env.NODE_ENV || "Ikke satt");
 
 const server = express();
 server.use(express.json());
 const port = process.env.PORT || 3000;
-
 server.set('port', port);
 
-server.use((req, res, next) => {
-    const ignoredPaths = [
-        "/favicon.ico",
-        "/js/",
-        "/css/",
-        "/icons/",
-        "/serviceWorker.js"
-    ];
+const ignoredPaths = [
+    "/favicon.ico",
+    "/js/",
+    "/css/",
+    "/icons/",
+    "/serviceWorker.js"
+];
 
+server.use((req, res, next) => {
     if (ignoredPaths.some(path => req.url.startsWith(path))) {
         return next();
     }
-    console.log(`[Vanguard] Sjekker tilgang til: ${req.url}`);
+
+    if (DEBUG_MODE) console.log(`[Vanguard] Sjekker tilgang til: ${req.url}`);
 
     for (const skill of vanguard.skills) {
         if (!skill.use(req, res)) {
-            console.log(`[Vanguard] Blokkerte tilgang til: ${req.url}`);
+            if (DEBUG_MODE) console.log(`[Vanguard] Blokkerte tilgang til: ${req.url}`);
             return;
         }
     }
@@ -64,7 +67,7 @@ server.get("*", (req, res) => {
 });
 
 server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    if (DEBUG_MODE) console.log(`Server running at http://localhost:${port}`);
 });
 
 export { server };
