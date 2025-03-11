e0439365e0f6ab1f773e69fbfa39ae3ba8eab593
sarahemmelin <sarahemmelin@gmail.com>
2025-03-11 03:53:31 +0100

    Har lagt til ny komponent (admin-view.mjs)

--- public/js/app.mjs
+++ public/js/app.mjs
@@ -4,6 +4,7 @@ import { checkLoginStatus } from "./view_controllers/loginController.mjs";
 import "/components/product-card.mjs";
 import "/components/add-product-form.mjs";
 import "/components/login-component.mjs";
+import "/components/admin-view.mjs";
 
 async function initiateApp() {
     registerServiceWorker();
