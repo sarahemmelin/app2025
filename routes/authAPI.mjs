2f2494f5f49306126e6b616c50f4f897378dfa2c
sarahemmelin <sarahemmelin@gmail.com>
2025-03-11 16:06:59 +0100

    Lagt til DEBUG_MODE

--- routes/authAPI.mjs
+++ routes/authAPI.mjs
@@ -1,20 +1,23 @@
 import express from "express";
 import { storeToken, verifyPassword } from "../modules/auth.mjs";
-import dotenv from "dotenv";
+import { DEBUG_MODE } from "../config/debug.mjs";
 
-// dotenv.config();
-console.log("[DEBUG] Render Environment Variables:");
+const router = express.Router();
+
+if (DEBUG_MODE){
+console.log("[DEBUG authAPI] Render Environment Variables:");
 console.log("ADMIN_EMAIL:", process.env.ADMIN_EMAIL);
 console.log("ADMIN_SALT:", process.env.ADMIN_SALT);
 console.log("ADMIN_HASH:", process.env.ADMIN_HASH);
-const router = express.Router();
+}
+
 
 function generateToken() {
   return Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
 }
 
 router.post("/login", (req, res) => {
-  console.log("[DEBUG] Mottatt req.body:", req.body);
+  if (DEBUG_MODE) console.log("[DEBUG authAPI] Mottatt req.body:", req.body);
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -26,31 +29,19 @@ router.post("/login", (req, res) => {
     salt: process.env.ADMIN_SALT,
     passwordHash: process.env.ADMIN_HASH
   };
-  
-  console.log("[DEBUG] Render Environment Variables:");
-  console.log("ADMIN_EMAIL:", process.env.ADMIN_EMAIL);
-  console.log("ADMIN_SALT:", process.env.ADMIN_SALT);
-  console.log("ADMIN_HASH:", process.env.ADMIN_HASH);
-
-  console.log("[DEBUG] Forventet admin-email:", admin.email);
-  console.log("[DEBUG] Bruker oppgitt email:", email);
 
   if (email !== admin.email) {
     console.error("[ERROR authAPI] Feil e-post!");
     return res.status(401).json({ message: "Feil brukernavn eller passord." });
   }
 
-  console.log("[DEBUG] Verifiserer passord...");
-
   if (!verifyPassword(password, admin.salt, admin.passwordHash)) {
     console.error("[ERROR authAPI] Feil passord!");
     return res.status(401).json({ message: "Feil brukernavn eller passord." });
   }
 
-  console.log("[DEBUG] Passord OK. Genererer token...");
-
   const token = generateToken();
-  console.log("[DEBUG] Innlogging vellykket. Token:", token);
+  if (DEBUG_MODE) console.log("[DEBUG authAPI] Innlogging vellykket. Token:", token);
   storeToken(token);
 
   res.json({ message: "Innlogging vellykket", token });
