11516a4d02b8739ae49d1c4ff8517822d657d7ef
sarahemmelin <sarahemmelin@gmail.com>
2025-03-14 03:04:11 +0100

    Lagt til PATCH i shopAPI

--- backend/routes/shopAPI.mjs
+++ backend/routes/shopAPI.mjs
@@ -1,13 +1,13 @@
 import express from "express";
 import { authenticateToken } from "../modules/auth.mjs";
-import { getAllProducts, getProduct, createProduct, deleteProduct } from "../controllers/shopController.mjs";
+import { getAllProducts, getProduct, createProduct, deleteProduct, updateProduct } from "../controllers/shopController.mjs";
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
-router.get("/products", getAllProducts);
-router.get("/products/:id", getProduct);
+router.get("/:id", getProduct);
 router.post("/", authenticateToken, createProduct);
+router.patch("/:id", authenticateToken, updateProduct);
 router.delete("/:id", authenticateToken, deleteProduct);
 
 export default router;
